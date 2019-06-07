1b119a05896aecb0894c81ccc73a0252d7899114
周志冉 <857827469@qq.com>
2019-06-07 23:53:16 +0800

    创建项目，并添加配置类与sqlalchemy

--- manage.py
+++ manage.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
+from redis import StrictRedis
 
 
 app = Flask(__name__)
@@ -7,13 +8,17 @@ app = Flask(__name__)
 class Config(object):
     DEBUG = True
     SQLALCHEMY_DATABASE_URI = "mysql://root:mysql@127.0.0.1:3306/flask_news"
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
+    REDIS_HOST = "127.0.0.1"
+    REDIS_PORT = 6379
 
 app.config.from_object(Config)
 db = SQLAlchemy(app)
+redis_store = StrictRedis(host=Config.REDIS_HOST,port=Config.REDIS_PORT)
 
 @app.route('/')
 def index():
+
     return 'index'
 
 if __name__ == '__main__':
