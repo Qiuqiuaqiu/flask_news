bc53d451bbb6f76e79361ff0fd4c89e4b29a87f8
周志冉 <857827469@qq.com>
2019-06-08 23:57:58 +0800

    渲染首页

--- info/modules/index/views.py
+++ info/modules/index/views.py
@@ -1,4 +1,4 @@
-from flask import session
+from flask import session, render_template
 from info.modules.index import index_blu
 # import logging
 from info import redis_store
@@ -6,6 +6,6 @@ from info import redis_store
 @index_blu.route('/')
 def index():
     # logging.error('error')
-    session["xxxx"] = "hahah"
-    redis_store.set("na","xiaohuahh")
-    return 'index'
+    # session["xxxx"] = "hahah"
+    # redis_store.set("na","xiaohuahh")
+    return render_template('news/index.html')
